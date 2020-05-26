3ff0ba721630ae2e64bcc23398b90864e9edb61f
Sundaram Srivastava <sundaram.srivastav007@gmail.com>
2020-05-26 07:24:21 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,6 @@
 # Write your #display_rainbow method here
+def greeting(person)
+  puts "name= #{person[0]}, age= #{person[1]}, occupation #{person[2]}"
+end
+bob =["sundu","20","coder"]
+greeting(bob)
